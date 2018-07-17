f5cdcf291291deb711e5799a6ee940271ab26cb4
Victor Velasquez <victorv24@gmail.com>
2018-07-17 16:40:57 -0700

    displaying attempts left

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
-
-
-let randomNumber = Math.floor(Math.random() * (commonWords.length))
-let randomWord = commonWords[randomNumber]
-
-
-let displayWord = document.getElementById('word')
-displayWord.innerHTML = randomWord
+let turnsLeft = 10
+let turnsTaken = 2 // hard coded for testing
+let underscore = ''
 
 
 
-function goo() {
-	let guess = document.getElementById('guess')
-	document.getElementById('guess-btn').onclick = function() {
-		guess.innerHTML = 'poop'
+
+// Generete random word to guess
+let randomNumber = commonWords[Math.floor(Math.random() * (commonWords.length))]
+
+
+
+// Calculate how many turns are left
+let attempts = function() {
+	if (turnsTaken > 0) {
+		turnsLeft = turnsLeft - turnsTaken
 	}
+	return turnsLeft
 }
-goo()
+
+
+
+for (let i = 0; i < randomNumber.length; i++) {
+	underscore += '_ '
+}
+
+
+// Displaying shit to screen
+// ==========================
+
+// Display the random word with underscores
+let displayWord = document.getElementById('word')
+displayWord.innerHTML = underscore 
+
+
+let displayAttempts = document.getElementById('attempts')
+displayAttempts.innerHTML = `you have ${attempts()} attempts left.`
