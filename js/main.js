6f456fb59f857f42e87a6c324be66f867562e049
Victor Velasquez <victorv24@gmail.com>
2018-07-19 16:11:36 -0700

    enhanced

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-
+/*
 
 let turnsLeft = 10 // counter to keep track of turns left
 let turnsTaken = 2 // turns player has taken will be stored here.
 let underscore = '' // used to place underscore on page
+let playerGuess = ''
 
-// Turns left
+// Keep tracks of turns left
 let attempts = function() {
 	if (turnsTaken > 0) {
 		turnsLeft = turnsLeft - turnsTaken
@@ -14,8 +15,11 @@ let attempts = function() {
 
 
 
-// Generete random word for player to guess
+// Generete random word 
 let randomWord = commonWords[ Math.floor(Math.random() * (commonWords.length)) ]
+console.log("randomWord:", randomWord)
+
+
 
 
 // Place underscore on the page
@@ -23,10 +27,29 @@ let underscoreToPage = function() {
 	for (let i = 0; i < randomWord.length; i++) {
 		underscore += '_ '
 	}
+	console.log('the printed underscores:', underscore)
 }
 
 
-// Displaying shit to screen
+
+document.querySelector('#guess-btn').addEventListener("click", function() {
+    for(let i = 0; i < randomWord.length; i++){
+        if(char.value == randomWord.charAt(i)){ 
+                let spl = underscore.split('')
+                    spl[i] = char.value
+                        underscore = spl.join('')
+        }
+    }
+    document.querySelector('#player-guess').value = ''
+    document.querySelector('#player-guess').focus()
+    document.querySelector('#player-guess').innerHTML = underscore
+})
+// document.querySelector('#correct').innerHTML = underscore
+// document.querySelector('#turns-remaining').innerHTML = maxTurns + " turns remaining"
+
+
+
+// Displaying things to the screen
 // ==========================
 
 // Display the random word with underscores
@@ -36,5 +59,63 @@ displayWord.innerHTML = underscoreToPage()
 // Display number of turns left to the screen
 let displayAttempts = document.getElementById('attempts')
 displayAttempts.innerHTML = `you have ${attempts()} attempts left.`
+*/
 
+// ==========================================================
 
+let randomWord = commonWords[Math.floor(Math.random() * commonWords.length)]
+var playerGuess = document.querySelector("#player-guess")
+var maxTurns      = 10
+var turnsTaken    = 0
+var currentIndex  = word
+var guessedLetters= []
+var hasFinished   = false
+var underscores   = ''
+
+console.log(`The random word of the day is... "${randomWord}"!`)
+
+// place underscores where letters are supposed to be
+for (var i = 0; i < randomWord.length; i += 1) {
+	underscores += '_ '
+}
+
+document.querySelector('#guess-btn').addEventListener("click", function() {
+	
+	for(let i = 0; i < randomWord.length; i++) {
+		
+		if (playerGuess.value == randomWord.charAt(i)) { 
+			
+			let splitWord = underscores.split('')
+			
+			splitWord[i] = playerGuess.value
+			underscores = splitWord.join('')
+
+		} else if (playerGuess.value != randomWord.charAt(i)) {
+			
+			turnsTaken++
+
+			if (turnsTaken > 0) {
+				maxTurns = maxTurns - turnsTaken
+			}
+		}
+
+	}
+
+	
+
+	document.querySelector('#player-guess').value = ''
+	document.querySelector('#player-guess').focus()
+	document.querySelector('#correct').innerHTML = underscores
+})
+
+// Keep tracks of turns left
+// let attempts = function() {
+// 	if (turnsTaken > 0) {
+	// 	maxTurns = maxTurns - turnsTaken
+	// }
+// 	console.log(maxTurns)
+// 	return maxTurns
+// }
+
+document.querySelector('#correct').innerHTML = underscores
+document.querySelector('#turns-remaining').innerHTML = maxTurns + " turns remaining"
